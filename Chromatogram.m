3987f54d420a126b76114c236da493c6d89b7b41
elkannn <163769993+elkannn@users.noreply.github.com>
2026-03-16 20:46:39 -0700

    Implement noise removal in chromatogram analysis
    Added noise filtering to chromatogram data and updated plots.

--- Chromatogram.m
+++ Chromatogram.m
@@ -1,8 +1,52 @@
+clear; clc; cla; clf
+
 [sampleStruct, probStruct, Comments] = scfread('sample.scf');
-figure
-hold on
-plot(sampleStruct.A);
-plot(sampleStruct.C);
-plot(sampleStruct.G);
-plot(sampleStruct.T);
-legend('A','C','G','T');
+% figure
+% hold on
+% plot(sampleStruct.A);
+% plot(sampleStruct.C);
+% plot(sampleStruct.G);
+% plot(sampleStruct.T);
+
+% legend('A','C','G','T');
+
+newSampleStructA = removeNoise(sampleStruct.A);
+newSampleStructC = removeNoise(sampleStruct.C);
+newSampleStructG = removeNoise(sampleStruct.G);
+newSampleStructT = removeNoise(sampleStruct.T);
+figure;
+hold on;
+plot(newSampleStructA);
+plot(newSampleStructC);
+plot(newSampleStructG);
+plot(newSampleStructT);
+legend('A (Filtered)', 'C (Filtered)', 'G (Filtered)', 'T (Filtered)');
+
+
+function newSampleStruct = removeNoise(sampleData)
+    startPoint = 1;
+    trimThreshold = mean(sampleData) * 0.1;
+
+    for i = 1:length(sampleData)
+        if sampleData(i) > trimThreshold
+            startPoint = i;
+            break;
+        end
+    end
+    
+    sampleData = sampleData(startPoint:end);
+
+    x = 1:length(sampleData);
+    x = x';
+
+    thresholdValue = mean(sampleData) * 0.7;
+    noiseIndices = find(sampleData < thresholdValue);
+    noiseValues = sampleData(noiseIndices);
+
+    p = polyfit(noiseIndices, noiseValues, 5);
+    noiseThreshold = polyval(p, x);
+
+    newSampleStruct = sampleData - noiseThreshold;
+
+    newSampleStruct(newSampleStruct < 0) = 0;
+end
